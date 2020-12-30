17491e9f7d86dc4a4a09ccdc682ab69d68b46ada
devamirgh <amir110gholizadeh@gmail.com>
2020-12-30 14:07:33 +0330

    refactor: seperate generating a single hash logic from main file

---
+++ src/genereateAHash.js
@@ -0,0 +1,10 @@
+const random = require("./random");
+module.exports = ({ prefix, suffix, length, keyset }) => {
+  let hash = "";
+  for (let i = 0; i < length; i++) {
+    const keys = keyset[random(0, keyset.length)];
+    const key = keys[random(0, keys.length)];
+    hash += key;
+  }
+  return prefix + hash + suffix;
+};
